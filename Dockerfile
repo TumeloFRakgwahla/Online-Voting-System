5bc35f2c62f4d1ccc2c1c3b75b07a660137014b3
TumeloFRakgwahla <tumelorakgwahla@gmail.com>
2025-06-06 09:36:20 +0200

    updated the dockerfile with php ext

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM php:8.2-apache
 
+# Install mysqli
+RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+
 # Copy everything into Apache web root
 COPY . /var/www/html/
 
--- admin/php/Tally_votes.php
+++ admin/php/Tally_votes.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection
-$conn = new mysqli("sql7.freesqldatabase.com", "sql7783315", "IzRkQwA8pR", "sql7783315");
+$conn = new mysqli('sql7.freesqldatabase.com', 'sql7783315', 'IzRkQwA8pR', 'sql7783315', 3306);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
